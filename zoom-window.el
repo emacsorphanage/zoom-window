f0eb12e389d8d2d13b5911907ef872e18230e00e
Syohei YOSHIDA <syohex@gmail.com>
2016-05-27 23:38:58 +0900

    Bump up version and update Changes

--- zoom-window.el
+++ zoom-window.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-zoom-window
-;; Version: 0.03
+;; Version: 0.04
 ;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
