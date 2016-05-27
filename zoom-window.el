90b9a161f470d1cf77b9b22169b0ab861146f3d5
Syohei YOSHIDA <syohex@gmail.com>
2016-05-27 23:37:20 +0900

    Add commentary section

--- zoom-window.el
+++ zoom-window.el
@@ -22,6 +22,10 @@
 
 ;;; Commentary:
 
+;; zoom-window.el provides functions which zooms specific window in frame and
+;; restore original window configuration. This is like tmux's zoom/unzoom
+;; features.
+
 ;;; Code:
 
 (require 'cl-lib)
